abf053e3a8397b231f27f95f930a6565b5ed383f
Steph974-Git <10466@holbertonstudents.com>
2025-04-21 22:19:41 +0200

    fix: Remove env_builtin function and its prototype from builtins.c and shell.h

--- builtins.c
+++ builtins.c
@@ -16,27 +16,3 @@ int exit_builtin(char **args)
 	}
 	return (0); /* Retourne 0 si ce n'est pas "exit" */
 }
-
-/**
- * env_builtin - Implémente la commande intégrée "env"
- * @args: Arguments de la commande (non utilisés pour env)
- *
- * Return: 1 si la commande est "env", 0 sinon
- */
-int env_builtin(char **args)
-{
-	int i = 0;
-
-	/* Vérifier si la commande est "env" */
-	if (args && args[0] && strcmp(args[0], "env") == 0)
-	{
-		/* Afficher chaque variable d'environnement */
-		while (environ[i])
-		{
-			printf("%s\n", environ[i]);
-			i++;
-		}
-		return (1); /* Commande traitée */
-	}
-	return (0); /* Ce n'est pas "env" */
-}
